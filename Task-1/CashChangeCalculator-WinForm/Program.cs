e0fa4b0cbae3ff8564a5a21a5f277a9e07999464
Alexander Lundqvist <33635814+KrimReaper@users.noreply.github.com>
2021-08-23 00:18:05 +0200

    Task 1 winform done

--- Task-1/CashChangeCalculator-WinForm/Program.cs
+++ Task-1/CashChangeCalculator-WinForm/Program.cs
@@ -23,73 +23,6 @@ namespace CashChangeCalculator_WinForm
 {
     static class Program
     {
-        private static void CalculateChange(int price, int paid)
-        {
-            int[] denominations = { 500, 200, 100, 50, 20, 10, 5, 1 };
-            int[] amountOfBills = { 0, 0, 0, 0, 0, 0, 0, 0 };
-            int change = paid - price;
-            if (change < 0)
-            {
-                Console.WriteLine("Payment was not enough, please try again.");
-            }
-            else
-            {
-                for (int i = 0; i < denominations.Length - 1; i++)
-                {
-                    if (change < denominations[i])
-                    {
-                        continue;
-                    }
-                    else
-                    {
-                        int newAmount = 0;
-                        int temp = change;
-                        while (temp >= denominations[i])
-                        {
-                            temp = temp / denominations[i];
-                            newAmount++;
-                        }
-                        amountOfBills[i] = newAmount;
-                        change %= denominations[i];
-                    }
-                }
-            }
-            amountOfBills[amountOfBills.Length - 1] = change; // Adding the last 1 SEK
-            ToString(amountOfBills);
-        }
-
-        private static void ToString(int[] result)
-        {
-            int[] denominations = { 500, 200, 100, 50, 20, 10, 5, 1 };
-            Console.WriteLine("\nChange back is:");
-
-            // Loop for bills
-            for (int i = 0; i < result.Length; i++)
-            {
-                if (result[i] == 0)
-                {
-                    continue;
-                }
-                else
-                {
-                    Console.WriteLine(result[i] + " x " + denominations[i] + billOrCoin(i));
-                }
-            }
-            Console.WriteLine();
-        }
-
-        private static string billOrCoin(int index)
-        {
-            if (index < 5)
-            {
-                return " SEK bills";
-            }
-            else
-            {
-                return " SEK coins";
-            }
-        }
-
         /// <summary>
         /// The main method that initializes and controlls the program flow.
         /// </summary>
@@ -100,42 +33,6 @@ namespace CashChangeCalculator_WinForm
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-
-
         }
     }
 }
-
-/*      
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Starting the change calculator...");
-
-            int price;
-            int amountPaid;
-
-            // Loop always running
-            while (true)
-            {
-                try
-                {
-                    Console.WriteLine("Write price to pay: ");
-                    price = Convert.ToInt32(Console.ReadLine());
-
-                    Console.WriteLine("Write amount paid: ");
-                    amountPaid = Convert.ToInt32(Console.ReadLine());
-                }
-
-                // Error handling as not to disturb the loop
-                catch (Exception e)
-                {
-                    Console.WriteLine("\n>>> Error: Invalid input format <<<\n");
-                    continue;
-                }
-                CalculateChange(price, amountPaid);
-            }
-
-        }
-    }
-}
-*/
--- Task-1/ChangeCalculatorForm/ChangeCalculatorForm/Form1.cs
+++ Task-1/ChangeCalculatorForm/ChangeCalculatorForm/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace CashChangeCalculator_WinForm
+namespace ChangeCalculatorForm
 {
     public partial class Form1 : Form
     {
@@ -17,17 +17,43 @@ namespace CashChangeCalculator_WinForm
             InitializeComponent();
         }
 
-        int price;
-        int amountPaid;
-
-        private static void CalculateChange(int price, int paid)
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int price = Convert.ToInt32(textBox1.Text);
+            int paid = Convert.ToInt32(textBox2.Text);
+            CalculateChange(price, paid);
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Clear()
+        {
+            richTextBox1.Text = "";
+        }
+
+        private void CalculateChange(int price, int paid)
+        {
+            Clear();
             int[] denominations = { 500, 200, 100, 50, 20, 10, 5, 1 };
             int[] amountOfBills = { 0, 0, 0, 0, 0, 0, 0, 0 };
             int change = paid - price;
             if (change < 0)
             {
-                Console.WriteLine("Payment was not enough, please try again.");
+                richTextBox1.AppendText("Payment was not enough, please try again.");
+                return;
             }
             else
             {
@@ -52,7 +78,26 @@ namespace CashChangeCalculator_WinForm
                 }
             }
             amountOfBills[amountOfBills.Length - 1] = change; // Adding the last 1 SEK
-            //ToString(amountOfBills);
+            ToString(amountOfBills);
+        }
+
+        private void ToString(int[] result)
+        {
+            int[] denominations = { 500, 200, 100, 50, 20, 10, 5, 1 };
+            richTextBox1.AppendText("Change back is:");
+
+            // Loop for bills
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (result[i] == 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    richTextBox1.AppendText("\n" + result[i] + " x " + denominations[i] + billOrCoin(i));
+                }
+            }
         }
 
         private static string billOrCoin(int index)
@@ -65,21 +110,6 @@ namespace CashChangeCalculator_WinForm
             {
                 return " SEK coins";
             }
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-            price = 0; 
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            //CalculateChange(price, paid);
         }
     }
 }
