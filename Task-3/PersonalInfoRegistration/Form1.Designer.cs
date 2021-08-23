1655d0bce0e22bb83da3851f5163c901699464b1
Alexander Lundqvist <33635814+KrimReaper@users.noreply.github.com>
2021-08-23 04:04:49 +0200

    Task 3 done
    Used task 1 as template. Working UI and you can use it any number of times. Didn't implement any error handling due to time constraints.

---
+++ Task-3/PersonalInfoRegistration/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿
+namespace PersonalInfoRegistration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(52, 78);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(341, 276);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "User input";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(69, 233);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(183, 233);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(255, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Write both name and social security number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "First name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Last name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Social security number";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Location = new System.Drawing.Point(416, 78);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(337, 276);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Result";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.richTextBox1.Location = new System.Drawing.Point(27, 41);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedBoth;
+            this.richTextBox1.Size = new System.Drawing.Size(284, 215);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(149, 74);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(161, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(149, 116);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(161, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(149, 158);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(161, 20);
+            this.textBox3.TabIndex = 10;
+            
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Personal information registration form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Task-3/PersonalInfoRegistration/person.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+namespace PersonalInfoRegistration
+{
+    // Class represents a person. It has fields for basic information about the person and methods
+    // to determine if a SSN is correct and which gender the person has.
+    public class person
+    {
+        // Fields for an instance of the class person
+        public string firstName;
+        public string lastName;
+        public string ssn;
+        private string ssnConverted;
+        private int[] parsedSSN;
+        public string gender;
+
+        // Constructor method 
+        public person(string firstName, string lastName, string ssn)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.ssn = ssn;
+            this.ssnConverted = ssn.Replace("-", "");
+            this.parsedSSN = ssnToArray();
+            this.gender = findGender();
+            
+        }
+
+        private int[] ssnToArray ()
+        {
+            // Converting the string into an array for better use in algorithm
+            parsedSSN = new int[this.ssnConverted.Length];
+            int counter = 0;
+            for (int i = 0; i < this.ssnConverted.Length; i++)
+            {
+                parsedSSN[i] = Convert.ToInt32(this.ssnConverted.Substring(counter, 1));
+                counter++;
+            }
+            return parsedSSN;
+        }
+
+        public bool correctSSN ()
+        {
+            bool answer;
+
+            int sum = 0;
+            int temp = 0;
+            for (int i = 0; i < this.parsedSSN.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    temp = this.parsedSSN[i] * 2;
+                    // Snippet bellow is because 2 digit numbers get summed
+                    if ( temp > 9 )
+                    {
+                        int a = temp / 10; // Get the "10th" digit
+                        int b = temp % 10; // Get the first digit
+                        temp = a + b;
+                    }
+                    sum += temp;
+                }
+                else
+                {
+                    sum += this.parsedSSN[i] * 1;
+                }
+            }
+
+            if (sum % 10 == 0)
+            {
+                answer = true;
+            }
+            else
+            {
+                answer = false;
+            }
+
+            return answer;
+        }
+
+        private string findGender ()
+        {
+            int genderValue = parsedSSN[parsedSSN.Length - 2];
+            string answer = "";
+            if (genderValue % 2 == 0)
+            {
+                answer = "Female";
+            }
+            else
+            {
+                answer = "Male";
+            }
+            return answer;
+        }
+
+        // Standard getter methods for the fields
+        public string getFirstName ()
+        {
+            return this.firstName;
+        }
+        public string getLastName()
+        {
+            return this.lastName;
+        }
+        public string getSSN()
+        {
+            return this.ssn;
+        }
+        public string getGender()
+        {
+            return this.gender;
+        }
+
+        //Delete this
+        public int[] getSSNParsed()
+        {
+            return this.parsedSSN;
+        }
+    }
+}
